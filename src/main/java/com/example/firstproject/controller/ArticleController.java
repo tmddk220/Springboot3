c7be8f814e5e8dec183af5fb88e9ef76b64586eb
tmddk220 <leeseunga1590@gmail.com>
2024-11-10 15:03:11 +0900

    View single data

--- src/main/java/com/example/firstproject/controller/ArticleController.java
+++ src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -6,8 +6,12 @@ import com.example.firstproject.repository.ArticleRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.Optional;
 
 @Slf4j
 @Controller
@@ -33,5 +37,14 @@ public class ArticleController {
         // System.out.println(saved.toString());
         return "";
     }
-
+    @GetMapping("/articles/{id}")
+    public String show(@PathVariable Long id, Model model) {
+        log.info("id = " + id); // id 잘 받았는지 확인하는 로그 찍기
+        // 1. id를 조회해 데이터 가져오기
+        Article articleEntity = articleRepository.findById(id).orElse(null);
+        // 2. 모델에 데이터 등록하기
+        model.addAttribute("article", articleEntity);
+        // 3. 뷰 페이지 반환하기
+        return "articles/show"; // 실제로 show.mustache 파일을 생성해야 함
+    }
 }
--- src/main/java/com/example/firstproject/entity/Article.java
+++ src/main/java/com/example/firstproject/entity/Article.java
@@ -5,9 +5,11 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 @AllArgsConstructor // Article() 생성자를 대체하는 어노테이션 추가
+@NoArgsConstructor  // 기본 생성자 추가 어노테이션
 @ToString           // toString() 메서드를 대체하는 어노테이션 추가
 @Entity
 public class Article {
@@ -18,8 +20,4 @@ public class Article {
     private String title;
     @Column
     private String content;
-
-    public Article() {
-
-    }
 }
