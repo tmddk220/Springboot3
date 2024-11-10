d070989c0fcec9f496e33ba7bff6977e459deccd
tmddk220 <leeseunga1590@gmail.com>
2024-11-10 17:59:29 +0900

    Edit post

--- src/main/java/com/example/firstproject/controller/ArticleController.java
+++ src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -54,4 +54,31 @@ public class ArticleController {
         // 3. 뷰 페이지 설정하기
         return "articles/index";
     }
+
+    @GetMapping("/articles/{id}/edit")  // 뷰 페이지에서 중괄호 2개, 컨트롤러에서 url 변수 사용할 땐 하나
+    public String edit(@PathVariable Long id, Model model) {
+        // 수정할 데이터 가져오기
+        Article articleEntity = articleRepository.findById(id).orElse(null);
+        // 모델에 데이터 등록하기
+        model.addAttribute("article", articleEntity);
+        // 뷰 페이지 설정하기
+        return "articles/edit";
+    }
+
+    @PostMapping("/articles/update")
+    public String update(ArticleForm form) {    // 매개변수로 DTO 받아 오기
+        log.info(form.toString());
+        // 1. DTO를 엔티티로 변환하기
+        Article articleEntity = form.toEntity();
+        log.info(articleEntity.toString());
+        // 2. 엔티티를 DB에 저장하기
+        // 2-1. DB에서 기존 데이터 받아오기
+        Article target = articleRepository.findById(articleEntity.getId()).orElse(null);
+        // 2-2. 기존 데이터 값을 갱신하기
+        if(target != null) {
+            articleRepository.save(articleEntity);  // 엔티티를 DB에 저장(갱신)
+        }
+        // 3. 수정 결과 페이지로 리다이렉트하기
+        return "redirect:/articles/" + articleEntity.getId();
+    }
 }
