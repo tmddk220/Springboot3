7fd5b5c826e132b872048a436618fb738950a416
tmddk220 <leeseunga1590@gmail.com>
2024-12-29 23:00:48 +0900

    Delete comment

--- src/main/java/com/example/firstproject/service/CommentService.java
+++ src/main/java/com/example/firstproject/service/CommentService.java
@@ -69,4 +69,15 @@ public class CommentService {
         // 4. 댓글 엔티티를 DTO로 변환 및 반환
         return CommentDto.createCommentDto(updated);
     }
+
+    public CommentDto delete(Long id) {
+        // 1. 댓글 조회 및 예외 발생
+        Comment target = commentRepository.findById(id)
+                .orElseThrow(()->new IllegalArgumentException("댓글 삭제 실패!" +
+                        "대상이 없습니다."));
+        // 2. 댓글 삭제
+        commentRepository.delete(target);
+        // 3. 삭제 댓글을 DTO로 변환 및 반환
+        return CommentDto.createCommentDto(target);
+    }
 }
